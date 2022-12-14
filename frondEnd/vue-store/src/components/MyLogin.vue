ea4cb24429da10726ee9b5e6bbc1fff82abc517b
Anhduchb01 <anhducythb01@gmail.com>
2022-12-14 23:52:54 +0700

    first commit

---
+++ frondEnd/vue-store/src/components/MyLogin.vue
@@ -0,0 +1,133 @@
+<!--
+ * @Description: Log In组件
+ * @Author: hai-27
+ * @Date: 2020-02-19 20:55:17
+ * @LastEditors: hai-27
+ * @LastEditTime: 2020-03-01 15:34:08
+ -->
+<template>
+  <div id="myLogin">
+    <el-dialog title="Log In" width="300px" center :visible.sync="isLogin">
+      <el-form :model="LoginUser" :rules="rules" status-icon ref="ruleForm" class="demo-ruleForm">
+        <el-form-item prop="name">
+          <el-input prefix-icon="el-icon-user-solid" placeholder="Please Enter Account" v-model="LoginUser.name"></el-input>
+        </el-form-item>
+        <el-form-item prop="pass">
+          <el-input
+            prefix-icon="el-icon-view"
+            type="password"
+            placeholder="Please Enter Password"
+            v-model="LoginUser.pass"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button size="medium" type="primary" @click="Login" style="width:100%;">Log In</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
+  </div>
+</template>
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  name: "MyLogin",
+  data() {
+    // 用户名的校验方法
+    let validateName = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("Please Enter User Name"));
+      }
+      // 用户名以字母开头,长度在5-16之间,允许字母数字下划线
+      const userNameRule = /^[a-zA-Z][a-zA-Z0-9_]{4,15}$/;
+      if (userNameRule.test(value)) {
+        this.$refs.ruleForm.validateField("checkPass");
+        return callback();
+      } else {
+        return callback(new Error("Start with a letter, length is between 5-16, alphanumeric underscore is allowed"));
+      }
+    };
+    // 密码的校验方法
+    let validatePass = (rule, value, callback) => {
+      if (value === "") {
+        return callback(new Error("Please Enter Password"));
+      }
+      // 密码以字母开头,长度在6-18之间,允许字母数字和下划线
+      const passwordRule = /^[a-zA-Z]\w{5,17}$/;
+      if (passwordRule.test(value)) {
+        this.$refs.ruleForm.validateField("checkPass");
+        return callback();
+      } else {
+        return callback(
+          new Error("Start with a letter, length between 6-18, allow alphanumeric and underscore")
+        );
+      }
+    };
+    return {
+      LoginUser: {
+        name: "",
+        pass: ""
+      },
+      // 用户信息校验规则,validator(校验方法),trigger(触发方式),blur为在组件 Input 失去焦点时触发
+      rules: {
+        name: [{ validator: validateName, trigger: "blur" }],
+        pass: [{ validator: validatePass, trigger: "blur" }]
+      }
+    };
+  },
+  computed: {
+    // 获取vuex中的showLogin，控制Log In组件是否显示
+    isLogin: {
+      get() {
+        return this.$store.getters.getShowLogin;
+      },
+      set(val) {
+        this.$refs["ruleForm"].resetFields();
+        this.setShowLogin(val);
+      }
+    }
+  },
+  methods: {
+    ...mapActions(["setUser", "setShowLogin"]),
+    Login() {
+      // 通过element自定义表单校验规则，校验用户输入的用户信息
+      this.$refs["ruleForm"].validate(valid => {
+        //如果通过校验开始Log In
+        if (valid) {
+          this.$axios
+            .post("/api/users/login", {
+              userName: this.LoginUser.name,
+              password: this.LoginUser.pass
+            })
+            .then(res => {
+              // “001”代表Log In成功，其他的均为失败
+              if (res.data.code === "001") {
+                // 隐藏Log In组件
+                this.isLogin = false;
+                // Log In信息存到本地
+                let user = JSON.stringify(res.data.user);
+                localStorage.setItem("user", user);
+                // Log In信息存到vuex
+                this.setUser(res.data.user);
+                // 弹出通知框提示Log In成功信息
+                this.notifySucceed(res.data.msg);
+              } else {
+                // 清空输入框的校验状态
+                this.$refs["ruleForm"].resetFields();
+                // 弹出通知框提示Log In失败信息
+                this.notifyError(res.data.msg);
+              }
+            })
+            .catch(err => {
+              return Promise.reject(err);
+            });
+        } else {
+          return false;
+        }
+      });
+    }
+  }
+};
+</script>
+<style>
+</style>
