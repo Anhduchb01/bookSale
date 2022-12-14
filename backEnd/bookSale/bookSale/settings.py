c40ff433e0865b8c07668033dc358233143d2131
Anhduchb01 <anhducythb01@gmail.com>
2022-12-15 00:58:55 +0700

    Connect mongodb

--- backEnd/bookSale/bookSale/settings.py
+++ backEnd/bookSale/bookSale/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,11 +76,11 @@ WSGI_APPLICATION = 'bookSale.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'booksale',
+        
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
---
+++ backEnd/bookSale/core/urls.py
@@ -0,0 +1,21 @@
+
+from django.urls import path
+# from .views import HomeView,TimKiem,load_quanhuyen,LoadTimKiem,Baidang,GetAllAPIBaidang, luubaidang,updatestar,daluu
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+#     path('', HomeView.as_view(),name='index'),
+#     path('timkiem', TimKiem.as_view(), name='timkiem'),
+#     path('ajax/load-quanhuyen/', load_quanhuyen.as_view(), name='ajax_load_quanhuyen'),
+#     path('timkiem/load',LoadTimKiem.as_view(), name="loadtimkiem"), 
+#     path('login',views.dangnhap, name="login"),
+#     path('dangky',views.dangky, name="dangky"),  
+#     path('dangxuat',views.dangxuat, name="dangxuat"),
+#     path('<int:id>',Baidang.as_view(), name="baidang"),
+#     path('Getbaidang/',GetAllAPIBaidang.as_view()),
+#     path('updatestar',views.updatestar,name='updatestar'),
+#     path('daluu/<str:username>',daluu.as_view(),name='daluu'),
+#     path('luubaidang',views.luubaidang,name='luubaidang')
+]
